0279e6276821085cba65195287c25dcec222a8eb
wyukawa <wyukawa@gmail.com>
2015-04-23 16:26:52 +0900

    delete velocity, backbone.js update bootstrap add table scroll

--- src/main/java/yanagishima/module/PrestoServletModule.java
+++ src/main/java/yanagishima/module/PrestoServletModule.java
@@ -1,6 +1,5 @@
 package yanagishima.module;
 
-import yanagishima.servlet.IndexServlet;
 import yanagishima.servlet.PrestoServlet;
 
 import com.google.inject.servlet.ServletModule;
@@ -9,10 +8,8 @@ public class PrestoServletModule extends ServletModule {
 	@Override
 	protected void configureServlets() {
 		bind(PrestoServlet.class);
-		bind(IndexServlet.class);
 
 		serve("/presto").with(PrestoServlet.class);
-		serve("/").with(IndexServlet.class);
 
 	}
 }
