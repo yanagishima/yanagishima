01b789fb2d41360c87e832be3471d20dbd032fd0
wyukawa <wyukawa@gmail.com>
2016-03-31 17:52:32 +0900

    format

--- src/main/java/yanagishima/service/PrestoServiceImpl.java
+++ src/main/java/yanagishima/service/PrestoServiceImpl.java
@@ -1,123 +1,111 @@
 package yanagishima.service;
 
-import static io.airlift.json.JsonCodec.jsonCodec;
-import static java.lang.String.format;
-import static java.util.concurrent.TimeUnit.MINUTES;
-
+import com.facebook.presto.client.*;
+import com.google.common.collect.Lists;
 import io.airlift.http.client.HttpClientConfig;
 import io.airlift.http.client.jetty.JettyHttpClient;
 import io.airlift.json.JsonCodec;
 import io.airlift.units.Duration;
-
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.IOException;
-import java.net.URI;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.sql.SQLException;
-import java.time.ZonedDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
-import java.util.TimeZone;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
-import java.nio.file.Files;
-
-import javax.inject.Inject;
-
 import me.geso.tinyorm.TinyORM;
 import yanagishima.config.YanagishimaConfig;
 import yanagishima.exception.QueryErrorException;
 import yanagishima.result.PrestoQueryResult;
-
-import com.facebook.presto.client.ClientSession;
-import com.facebook.presto.client.Column;
-import com.facebook.presto.client.QueryError;
-import com.facebook.presto.client.QueryResults;
-import com.facebook.presto.client.StatementClient;
-import com.google.common.collect.Lists;
 import yanagishima.row.Query;
+
+import javax.inject.Inject;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.SQLException;
+import java.time.ZonedDateTime;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import static io.airlift.json.JsonCodec.jsonCodec;
+import static java.lang.String.format;
+import static java.util.concurrent.TimeUnit.MINUTES;
 
 public class PrestoServiceImpl implements PrestoService {
 
-	private YanagishimaConfig yanagishimaConfig;
+    private YanagishimaConfig yanagishimaConfig;
 
-	private JettyHttpClient httpClient;
+    private JettyHttpClient httpClient;
 
-	@Inject
-	private TinyORM db;
+    @Inject
+    private TinyORM db;
 
-	@Inject
-	public PrestoServiceImpl(YanagishimaConfig yanagishimaConfig) {
-		this.yanagishimaConfig = yanagishimaConfig;
-		HttpClientConfig httpClientConfig = new HttpClientConfig().setConnectTimeout(new Duration(10, TimeUnit.SECONDS));
-		this.httpClient = new JettyHttpClient(httpClientConfig);
-	}
+    @Inject
+    public PrestoServiceImpl(YanagishimaConfig yanagishimaConfig) {
+        this.yanagishimaConfig = yanagishimaConfig;
+        HttpClientConfig httpClientConfig = new HttpClientConfig().setConnectTimeout(new Duration(10, TimeUnit.SECONDS));
+        this.httpClient = new JettyHttpClient(httpClientConfig);
+    }
 
-	@Override
-	public PrestoQueryResult doQuery(String query) throws QueryErrorException {
+    @Override
+    public PrestoQueryResult doQuery(String query) throws QueryErrorException {
 
-		try (StatementClient client = getStatementClient(query)) {
-			while (client.isValid() && (client.current().getData() == null)) {
-				client.advance();
-			}
+        try (StatementClient client = getStatementClient(query)) {
+            while (client.isValid() && (client.current().getData() == null)) {
+                client.advance();
+            }
 
-			if ((!client.isFailed()) && (!client.isGone()) && (!client.isClosed())) {
-				QueryResults results = client.isValid() ? client.current() : client.finalResults();
-				String queryId = results.getId();
-				db.insert(Query.class)
-						.value("query_id", queryId)
-						.value("fetch_result_time_string", ZonedDateTime.now().toString())
-						.value("query_string", query)
-						.execute();
-				if (results.getUpdateType() != null) {
-					PrestoQueryResult prestoQueryResult = new PrestoQueryResult();
-					prestoQueryResult.setQueryId(queryId);
-					prestoQueryResult.setUpdateType(results.getUpdateType());
-					return prestoQueryResult;
-				} else if (results.getColumns() == null) {
-					throw new QueryErrorException(new SQLException(format("Query %s has no columns\n", results.getId())));
-				} else {
-					PrestoQueryResult prestoQueryResult = new PrestoQueryResult();
-					prestoQueryResult.setQueryId(queryId);
-					prestoQueryResult.setUpdateType(results.getUpdateType());
-					List<String> columns = Lists.transform(results.getColumns(), Column::getName);
-					prestoQueryResult.setColumns(columns);
-					List<List<String>> rowDataList = new ArrayList<List<String>>();
-					processData(client, queryId, prestoQueryResult, columns, rowDataList);
-					prestoQueryResult.setRecords(rowDataList);
-					return prestoQueryResult;
-				}
-			}
+            if ((!client.isFailed()) && (!client.isGone()) && (!client.isClosed())) {
+                QueryResults results = client.isValid() ? client.current() : client.finalResults();
+                String queryId = results.getId();
+                db.insert(Query.class)
+                        .value("query_id", queryId)
+                        .value("fetch_result_time_string", ZonedDateTime.now().toString())
+                        .value("query_string", query)
+                        .execute();
+                if (results.getUpdateType() != null) {
+                    PrestoQueryResult prestoQueryResult = new PrestoQueryResult();
+                    prestoQueryResult.setQueryId(queryId);
+                    prestoQueryResult.setUpdateType(results.getUpdateType());
+                    return prestoQueryResult;
+                } else if (results.getColumns() == null) {
+                    throw new QueryErrorException(new SQLException(format("Query %s has no columns\n", results.getId())));
+                } else {
+                    PrestoQueryResult prestoQueryResult = new PrestoQueryResult();
+                    prestoQueryResult.setQueryId(queryId);
+                    prestoQueryResult.setUpdateType(results.getUpdateType());
+                    List<String> columns = Lists.transform(results.getColumns(), Column::getName);
+                    prestoQueryResult.setColumns(columns);
+                    List<List<String>> rowDataList = new ArrayList<List<String>>();
+                    processData(client, queryId, prestoQueryResult, columns, rowDataList);
+                    prestoQueryResult.setRecords(rowDataList);
+                    return prestoQueryResult;
+                }
+            }
 
-			if (client.isClosed()) {
-				throw new RuntimeException("Query aborted by user");
-			} else if (client.isGone()) {
-				throw new RuntimeException("Query is gone (server restarted?)");
-			} else if (client.isFailed()) {
-				throw resultsException(client.finalResults());
-			}
-			
-		}
-		throw new RuntimeException("should not reach");
+            if (client.isClosed()) {
+                throw new RuntimeException("Query aborted by user");
+            } else if (client.isGone()) {
+                throw new RuntimeException("Query is gone (server restarted?)");
+            } else if (client.isFailed()) {
+                throw resultsException(client.finalResults());
+            }
 
-	}
+        }
+        throw new RuntimeException("should not reach");
 
-	private void processData(StatementClient client, String queryId, PrestoQueryResult prestoQueryResult, List<String> columns, List<List<String>> rowDataList) {
-		int limit = yanagishimaConfig.getSelectLimit();
-		String currentPath = new File(".").getAbsolutePath();
-		String yyyymmdd = queryId.substring(0, 8);
-		File yyyymmddDir = new File(String.format("%s/result/%s", currentPath, yyyymmdd));
-		if(!yyyymmddDir.isDirectory()) {
-			yyyymmddDir.mkdir();
-		}
-		Path dst = Paths.get(String.format("%s/result/%s/%s.tsv", currentPath, yyyymmdd, queryId));
-		try (BufferedWriter bw = Files.newBufferedWriter(dst, StandardCharsets.UTF_8)) {
+    }
+
+    private void processData(StatementClient client, String queryId, PrestoQueryResult prestoQueryResult, List<String> columns, List<List<String>> rowDataList) {
+        int limit = yanagishimaConfig.getSelectLimit();
+        String currentPath = new File(".").getAbsolutePath();
+        String yyyymmdd = queryId.substring(0, 8);
+        File yyyymmddDir = new File(String.format("%s/result/%s", currentPath, yyyymmdd));
+        if (!yyyymmddDir.isDirectory()) {
+            yyyymmddDir.mkdir();
+        }
+        Path dst = Paths.get(String.format("%s/result/%s/%s.tsv", currentPath, yyyymmdd, queryId));
+        try (BufferedWriter bw = Files.newBufferedWriter(dst, StandardCharsets.UTF_8)) {
             bw.write(String.join("\t", columns));
             bw.write("\n");
             while (client.isValid()) {
@@ -127,12 +115,12 @@ public class PrestoServiceImpl implements PrestoService {
                         List<String> columnDataList = new ArrayList<>();
                         List<Object> tmpColumnDataList = row.stream().collect(Collectors.toList());
                         for (Object tmpColumnData : tmpColumnDataList) {
-                            if(tmpColumnData instanceof Long) {
+                            if (tmpColumnData instanceof Long) {
                                 columnDataList.add(((Long) tmpColumnData).toString());
                             } else {
-                                if(tmpColumnData == null) {
+                                if (tmpColumnData == null) {
                                     columnDataList.add(null);
-                                } else{
+                                } else {
                                     columnDataList.add(tmpColumnData.toString());
                                 }
                             }
@@ -143,11 +131,11 @@ public class PrestoServiceImpl implements PrestoService {
                         } catch (IOException e) {
                             throw new RuntimeException(e);
                         }
-						if(rowDataList.size() < limit) {
-							rowDataList.add(columnDataList);
-						} else {
-							prestoQueryResult.setWarningMessage(String.format("now fetch size is %d. This is more than %d. So, fetch operation stopped.", rowDataList.size(), limit));
-						}
+                        if (rowDataList.size() < limit) {
+                            rowDataList.add(columnDataList);
+                        } else {
+                            prestoQueryResult.setWarningMessage(String.format("now fetch size is %d. This is more than %d. So, fetch operation stopped.", rowDataList.size(), limit));
+                        }
                     });
                 }
                 client.advance();
@@ -155,30 +143,30 @@ public class PrestoServiceImpl implements PrestoService {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-	}
+    }
 
-	private StatementClient getStatementClient(String query) {
-		String prestoCoordinatorServer = yanagishimaConfig
-				.getPrestoCoordinatorServer();
-		String catalog = yanagishimaConfig.getCatalog();
-		String schema = yanagishimaConfig.getSchema();
-		String user = yanagishimaConfig.getUser();
-		String source = yanagishimaConfig.getSource();
+    private StatementClient getStatementClient(String query) {
+        String prestoCoordinatorServer = yanagishimaConfig
+                .getPrestoCoordinatorServer();
+        String catalog = yanagishimaConfig.getCatalog();
+        String schema = yanagishimaConfig.getSchema();
+        String user = yanagishimaConfig.getUser();
+        String source = yanagishimaConfig.getSource();
 
-		JsonCodec<QueryResults> jsonCodec = jsonCodec(QueryResults.class);
+        JsonCodec<QueryResults> jsonCodec = jsonCodec(QueryResults.class);
 
-		ClientSession clientSession = new ClientSession(
-				URI.create(prestoCoordinatorServer), user, source, catalog,
-				schema, TimeZone.getDefault().getID(), Locale.getDefault(),
-				new HashMap<String, String>(), null, false, new Duration(2, MINUTES));
-		return new StatementClient(httpClient, jsonCodec, clientSession, query);
-	}
+        ClientSession clientSession = new ClientSession(
+                URI.create(prestoCoordinatorServer), user, source, catalog,
+                schema, TimeZone.getDefault().getID(), Locale.getDefault(),
+                new HashMap<String, String>(), null, false, new Duration(2, MINUTES));
+        return new StatementClient(httpClient, jsonCodec, clientSession, query);
+    }
 
-	private QueryErrorException resultsException(QueryResults results) {
-		QueryError error = results.getError();
-		String message = format("Query failed (#%s): %s", results.getId(), error.getMessage());
-		Throwable cause = (error.getFailureInfo() == null) ? null : error.getFailureInfo().toException();
-		return new QueryErrorException(error, new SQLException(message, error.getSqlState(), error.getErrorCode(), cause));
-	}
+    private QueryErrorException resultsException(QueryResults results) {
+        QueryError error = results.getError();
+        String message = format("Query failed (#%s): %s", results.getId(), error.getMessage());
+        Throwable cause = (error.getFailureInfo() == null) ? null : error.getFailureInfo().toException();
+        return new QueryErrorException(error, new SQLException(message, error.getSqlState(), error.getErrorCode(), cause));
+    }
 
 }
--- src/main/java/yanagishima/servlet/HistoryServlet.java
+++ src/main/java/yanagishima/servlet/HistoryServlet.java
@@ -1,9 +1,5 @@
 package yanagishima.servlet;
 
-import com.facebook.presto.sql.SqlFormatter;
-import com.facebook.presto.sql.parser.ParsingException;
-import com.facebook.presto.sql.parser.SqlParser;
-import com.facebook.presto.sql.tree.Statement;
 import me.geso.tinyorm.TinyORM;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,57 +25,57 @@ import java.util.*;
 @Singleton
 public class HistoryServlet extends HttpServlet {
 
-	private static Logger LOGGER = LoggerFactory
-			.getLogger(HistoryServlet.class);
+    private static Logger LOGGER = LoggerFactory
+            .getLogger(HistoryServlet.class);
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Inject
-	private TinyORM db;
+    @Inject
+    private TinyORM db;
 
-	private YanagishimaConfig yanagishimaConfig;
+    private YanagishimaConfig yanagishimaConfig;
 
-	@Inject
-	public HistoryServlet(YanagishimaConfig yanagishimaConfig) {
-		this.yanagishimaConfig = yanagishimaConfig;
-	}
+    @Inject
+    public HistoryServlet(YanagishimaConfig yanagishimaConfig) {
+        this.yanagishimaConfig = yanagishimaConfig;
+    }
 
-	@Override
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+    @Override
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) throws ServletException, IOException {
 
-		HashMap<String, Object> retVal = new HashMap<String, Object>();
+        HashMap<String, Object> retVal = new HashMap<String, Object>();
 
-		try {
-			Optional<String> queryidOptional = Optional.ofNullable(request.getParameter("queryid"));
-			queryidOptional.ifPresent(queryid -> {
-				setQueryString(retVal, queryid);
-				setResults(retVal, queryid);
-			});
-		} catch (Throwable e) {
-			LOGGER.error(e.getMessage(), e);
-			retVal.put("error", e.getMessage());
-		}
+        try {
+            Optional<String> queryidOptional = Optional.ofNullable(request.getParameter("queryid"));
+            queryidOptional.ifPresent(queryid -> {
+                setQueryString(retVal, queryid);
+                setResults(retVal, queryid);
+            });
+        } catch (Throwable e) {
+            LOGGER.error(e.getMessage(), e);
+            retVal.put("error", e.getMessage());
+        }
 
-		JsonUtil.writeJSON(response, retVal);
+        JsonUtil.writeJSON(response, retVal);
 
-	}
+    }
 
-	private void setResults(HashMap<String, Object> retVal, String queryid) {
-		int limit = yanagishimaConfig.getSelectLimit();
-		String currentPath = new File(".").getAbsolutePath();
-		String yyyymmdd = queryid.substring(0, 8);
-		Path src = Paths.get(String.format("%s/result/%s/%s.tsv", currentPath, yyyymmdd, queryid));
-		List<List<String>> rowDataList = new ArrayList<List<String>>();
-		try (BufferedReader br = Files.newBufferedReader(src, StandardCharsets.UTF_8)) {
+    private void setResults(HashMap<String, Object> retVal, String queryid) {
+        int limit = yanagishimaConfig.getSelectLimit();
+        String currentPath = new File(".").getAbsolutePath();
+        String yyyymmdd = queryid.substring(0, 8);
+        Path src = Paths.get(String.format("%s/result/%s/%s.tsv", currentPath, yyyymmdd, queryid));
+        List<List<String>> rowDataList = new ArrayList<List<String>>();
+        try (BufferedReader br = Files.newBufferedReader(src, StandardCharsets.UTF_8)) {
             String line = br.readLine();
             int lineNumber = 0;
-            while(line != null){
-                if(lineNumber == 0) {
+            while (line != null) {
+                if (lineNumber == 0) {
                     String[] columns = line.split("\t");
                     retVal.put("headers", Arrays.asList(columns));
                 } else {
-                    if(lineNumber <= limit) {
+                    if (lineNumber <= limit) {
                         String[] row = line.split("\t");
                         rowDataList.add(Arrays.asList(row));
                     } else {
@@ -93,11 +89,11 @@ public class HistoryServlet extends HttpServlet {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-		retVal.put("results", rowDataList);
-	}
+        retVal.put("results", rowDataList);
+    }
 
-	private void setQueryString(HashMap<String, Object> retVal, String queryid) {
-		try {
+    private void setQueryString(HashMap<String, Object> retVal, String queryid) {
+        try {
             Optional<Query> queryOptional = db.single(Query.class).where("query_id=?", queryid).execute();
             queryOptional.ifPresent(query -> {
                 retVal.put("queryString", query.getQueryString());
@@ -105,6 +101,6 @@ public class HistoryServlet extends HttpServlet {
         } catch (Throwable e) {
             throw new RuntimeException(e);
         }
-	}
+    }
 
 }
