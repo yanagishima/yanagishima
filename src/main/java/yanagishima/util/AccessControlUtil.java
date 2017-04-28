1a77ae92f8043b233d737f442af60c71ac642cce
wyukawa <wyukawa@gmail.com>
2017-04-28 17:22:19 +0900

    tune on security feature

--- src/main/java/yanagishima/util/AccessControlUtil.java
+++ src/main/java/yanagishima/util/AccessControlUtil.java
@@ -11,14 +11,14 @@ public class AccessControlUtil {
 
     public static void checkDatasource(HttpServletRequest request, String datasource) {
 
-//        String header = HttpRequestUtil.getHeader(request, Constants.DATASOURCE_HEADER);
-//        if(header.equals("*")) {
-//            return;
-//        }
-//        List<String> headerDatasources = Arrays.asList(header.split(","));
-//        if(!headerDatasources.contains(datasource)) {
-//            throw new RuntimeException("Denied access to " + datasource);
-//        }
+        String header = HttpRequestUtil.getHeader(request, Constants.DATASOURCE_HEADER);
+        if(header.equals("*")) {
+            return;
+        }
+        List<String> headerDatasources = Arrays.asList(header.split(","));
+        if(!headerDatasources.contains(datasource)) {
+            throw new RuntimeException("Denied access to " + datasource);
+        }
     }
 
 }
