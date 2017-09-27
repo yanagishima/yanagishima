13bc57d43030b9ed5899cbff3cd791691d01ceeb
wyukawa <wyukawa@gmail.com>
2017-09-27 15:55:23 +0900

    Use commons-csv

--- src/main/java/yanagishima/util/DownloadUtil.java
+++ src/main/java/yanagishima/util/DownloadUtil.java
@@ -2,11 +2,11 @@ package yanagishima.util;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.nio.file.Files;
 import java.util.List;
 
@@ -15,13 +15,14 @@ public class DownloadUtil {
     public static void tsvDownload(HttpServletResponse response, String fileName, String datasource, String queryid) {
         response.setContentType("application/octet-stream");
         response.setHeader("Content-Disposition", "inline; filename=\"" + fileName + "\"");
-        try (OutputStream out = response.getOutputStream()) {
-            try(BufferedReader br = Files.newBufferedReader(PathUtil.getResultFilePath(datasource, queryid, false))) {
+        try (PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(response.getOutputStream(), "UTF-8"))) {
+            try(BufferedReader br = Files.newBufferedReader(PathUtil.getResultFilePath(datasource, queryid, false));
+                CSVPrinter csvPrinter = new CSVPrinter(printWriter, CSVFormat.EXCEL.withDelimiter('\t').withRecordSeparator(System.getProperty("line.separator")));) {
                 br.lines().forEach(line -> {
                     try {
                         ObjectMapper mapper = new ObjectMapper();
                         List row = mapper.readValue(line, List.class);
-                        out.write((String.join("\t", row) + System.getProperty("line.separator")).getBytes("UTF-8"));
+                        csvPrinter.printRecord(row);
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
@@ -35,13 +36,14 @@ public class DownloadUtil {
     public static void csvDownload(HttpServletResponse response, String fileName, String datasource, String queryid) {
         response.setContentType("text/csv; charset=Shift_JIS");
         response.setHeader("Content-Disposition", "inline; filename=\"" + fileName + "\"");
-        try (OutputStream out = response.getOutputStream()) {
-            try(BufferedReader br = Files.newBufferedReader(PathUtil.getResultFilePath(datasource, queryid, false))) {
+        try (PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(response.getOutputStream(), "Shift_JIS"))) {
+            try(BufferedReader br = Files.newBufferedReader(PathUtil.getResultFilePath(datasource, queryid, false));
+                CSVPrinter csvPrinter = new CSVPrinter(printWriter, CSVFormat.EXCEL.withRecordSeparator(System.getProperty("line.separator")));) {
                 br.lines().forEach(line -> {
                     try {
                         ObjectMapper mapper = new ObjectMapper();
                         List row = mapper.readValue(line, List.class);
-                        out.write((String.join(",", row) + System.getProperty("line.separator")).getBytes("Shift_JIS"));
+                        csvPrinter.printRecord(row);
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
