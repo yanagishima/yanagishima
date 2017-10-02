45c2ac1733d352548b08cb9bbdbaecb4310b639f
wyukawa <wyukawa@gmail.com>
2017-10-02 16:20:28 +0900

    Revert "set Access-Control-Allow-Headers to *"
    This reverts commit dbe2d8f7c94d62587651a393743c513540311685.

--- src/main/java/yanagishima/filter/YanagishimaFilter.java
+++ src/main/java/yanagishima/filter/YanagishimaFilter.java
@@ -1,6 +1,9 @@
 package yanagishima.filter;
 
+import yanagishima.util.Constants;
+
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -13,7 +16,7 @@ public class YanagishimaFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse res = (HttpServletResponse) response;
-        res.setHeader("Access-Control-Allow-Headers", "*");
+        res.setHeader("Access-Control-Allow-Headers", Constants.DATASOURCE_HEADER);
         res.setHeader("Access-Control-Allow-Origin", "*");
         chain.doFilter(request, response);
     }
