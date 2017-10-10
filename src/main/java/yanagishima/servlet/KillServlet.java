f0085f9363c9bb6c41d52676933ea5eba1377be4
wyukawa <wyukawa@gmail.com>
2017-10-10 15:52:57 +0900

    Add auth logic to kill

--- src/main/java/yanagishima/servlet/KillServlet.java
+++ src/main/java/yanagishima/servlet/KillServlet.java
@@ -1,11 +1,13 @@
 package yanagishima.servlet;
 
-import org.apache.http.client.fluent.Request;
+import okhttp3.OkHttpClient;
+import okhttp3.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import yanagishima.config.YanagishimaConfig;
 import yanagishima.util.AccessControlUtil;
 import yanagishima.util.HttpRequestUtil;
+import yanagishima.util.JsonUtil;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -14,8 +16,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Optional;
 
+import static com.facebook.presto.client.OkHttpUtil.basicAuth;
 import static javax.servlet.http.HttpServletResponse.SC_FORBIDDEN;
 
 @Singleton
@@ -27,6 +31,8 @@ public class KillServlet extends HttpServlet {
 
 	private YanagishimaConfig yanagishimaConfig;
 
+	private OkHttpClient httpClient = new OkHttpClient();
+
 	@Inject
 	public KillServlet(YanagishimaConfig yanagishimaConfig) {
 		this.yanagishimaConfig = yanagishimaConfig;
@@ -34,7 +40,15 @@ public class KillServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest request,
+						 HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
+
+		HashMap<String, Object> retVal = new HashMap<String, Object>();
 
 		Optional<String> queryIdOptinal = Optional.ofNullable(request.getParameter("queryid"));
 		queryIdOptinal.ifPresent(queryId -> {
@@ -49,12 +63,37 @@ public class KillServlet extends HttpServlet {
 					}
 				}
 			}
-			String prestoCoordinatorServer = yanagishimaConfig.getPrestoCoordinatorServer(datasource);
 			try {
-				Request.Delete(prestoCoordinatorServer + "/v1/query/" + queryId).execute();
-			} catch (IOException e) {
-				throw new RuntimeException(e);
+				String prestoCoordinatorServer = yanagishimaConfig.getPrestoCoordinatorServer(datasource);
+				okhttp3.Request prestoRequest = new okhttp3.Request.Builder().url(prestoCoordinatorServer + "/v1/query/" + queryId).delete().build();
+				Optional<String> prestoUser = Optional.ofNullable(request.getParameter("presto_user"));
+				Optional<String> prestoPassword = Optional.ofNullable(request.getParameter("presto_password"));
+				Response prestoResponse = null;
+				if (prestoUser.isPresent() && prestoPassword.isPresent()) {
+					OkHttpClient.Builder clientBuilder = httpClient.newBuilder();
+					clientBuilder.addInterceptor(basicAuth(prestoUser.get(), prestoPassword.get()));
+					try {
+						prestoResponse = clientBuilder.build().newCall(prestoRequest).execute();
+					} catch (IOException e) {
+						throw new RuntimeException(e);
+					}
+				} else {
+					try {
+						prestoResponse = httpClient.newCall(prestoRequest).execute();
+					} catch (IOException e) {
+						throw new RuntimeException(e);
+					}
+				}
+				retVal.put("code", prestoResponse.code());
+				retVal.put("message", prestoResponse.message());
+				retVal.put("url", prestoResponse.request().url());
+			} catch (Throwable e) {
+				LOGGER.error(e.getMessage(), e);
+				retVal.put("error", e.getMessage());
 			}
+
+			JsonUtil.writeJSON(response, retVal);
+
 		});
 
 	}
