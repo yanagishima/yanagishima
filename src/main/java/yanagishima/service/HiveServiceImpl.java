d8b42f2a95c40b2904588477f1616f59780c0785
wyukawa <wyukawa@gmail.com>
2019-02-25 10:53:58 +0900

    this is a hive only logic

--- src/main/java/yanagishima/service/HiveServiceImpl.java
+++ src/main/java/yanagishima/service/HiveServiceImpl.java
@@ -206,16 +206,18 @@ public class HiveServiceImpl implements HiveService {
         try (Statement statement = connection.createStatement()) {
             int timeout = (int) queryMaxRunTime.toMillis() / 1000;
             statement.setQueryTimeout(timeout);
-            String jobName = null;
-            if (userName == null) {
-                jobName = YANAGISHIAM_HIVE_JOB_PREFIX + queryId;
-            } else {
-                jobName = YANAGISHIAM_HIVE_JOB_PREFIX + userName + "-" + queryId;
-            }
-            statement.execute("set mapreduce.job.name=" + jobName);
-            List<String> hiveSetupQueryList = this.yanagishimaConfig.getHiveSetupQueryList(datasource);
-            for (String hiveSetupQuery : hiveSetupQueryList) {
-                statement.execute(hiveSetupQuery);
+            if(engine.equals("hive")) {
+                String jobName = null;
+                if (userName == null) {
+                    jobName = YANAGISHIAM_HIVE_JOB_PREFIX + queryId;
+                } else {
+                    jobName = YANAGISHIAM_HIVE_JOB_PREFIX + userName + "-" + queryId;
+                }
+                statement.execute("set mapreduce.job.name=" + jobName);
+                List<String> hiveSetupQueryList = this.yanagishimaConfig.getHiveSetupQueryList(datasource);
+                for (String hiveSetupQuery : hiveSetupQueryList) {
+                    statement.execute(hiveSetupQuery);
+                }
             }
 
             if (async && yanagishimaConfig.isUseJdbcCancel(datasource)) {
