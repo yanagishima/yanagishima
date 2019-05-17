c88c3eec72e86ccb1d687a1f1b87a7e7429664fd
wyukawa <wyukawa@gmail.com>
2019-05-17 13:02:37 +0900

    improve query history fetch logic

--- src/main/java/yanagishima/servlet/QueryHistoryUserServlet.java
+++ src/main/java/yanagishima/servlet/QueryHistoryUserServlet.java
@@ -65,25 +65,25 @@ public class QueryHistoryUserServlet extends HttpServlet {
 
             retVal.put("headers", Arrays.asList("Id", "Query", "Time", "rawDataSize", "engine", "finishedTime", "linenumber", "labelName"));
 
+            String limit = Optional.ofNullable(request.getParameter("limit")).orElse("100");
             String label = request.getParameter("label");
             List<Query> queryList;
             if(label == null || label.length() == 0) {
-                queryList = db.searchBySQL(Query.class,
-                        "SELECT a.engine, a.query_id, a.fetch_result_time_string, a.query_string, a.status, a.elapsed_time_millis, a.result_file_size, a.linenumber, b.label_name AS label_name " +
-                                "FROM query a LEFT OUTER JOIN label b on a.datasource = b.datasource AND a.engine = b.engine AND a.query_id = b.query_id " +
-                                "WHERE a.datasource=\'" + datasource + "\' and a.engine=\'" + engine + "\' and a.user=\'" + userName + "\' and a.query_string LIKE '%" + Optional.ofNullable(search).orElse("") + "%' ORDER BY a.query_id DESC");
+                String joinWhere = "LEFT OUTER JOIN label b on a.datasource = b.datasource AND a.engine = b.engine AND a.query_id = b.query_id WHERE a.datasource=\'" + datasource + "\' and a.engine=\'" + engine + "\' and a.user=\'" + userName + "\' and a.status != 'FAILED' and a.query_string LIKE '%" + Optional.ofNullable(search).orElse("") + "%' ORDER BY a.query_id DESC LIMIT " + limit;
+                String countSql = "SELECT count(*) FROM query a " + joinWhere;
+                String fetchSql = "SELECT a.engine, a.query_id, a.fetch_result_time_string, a.query_string, a.status, a.elapsed_time_millis, a.result_file_size, a.linenumber, b.label_name AS label_name FROM query a " + joinWhere;
+                retVal.put("hit", db.queryForLong(countSql).getAsLong());
+                queryList = db.searchBySQL(Query.class, fetchSql);
             } else {
                 queryList = db.searchBySQL(Query.class,
                         "SELECT a.engine, a.query_id, a.fetch_result_time_string, a.query_string, a.status, a.elapsed_time_millis, a.result_file_size, a.linenumber, b.label_name AS label_name " +
-                                "FROM query a LEFT OUTER JOIN label b on a.datasource = b.datasource AND a.engine = b.engine AND a.query_id = b.query_id WHERE b.label_name = \'" + label
-                                + "\' and a.datasource=\'" + datasource + "\' and a.engine=\'" + engine + "\' and a.user=\'" + userName + "\'");
+                                "FROM query a LEFT OUTER JOIN label b on a.datasource = b.datasource AND a.engine = b.engine AND a.query_id = b.query_id WHERE a.status != 'FAILED' and b.label_name = \'" + label
+                                + "\' and a.datasource=\'" + datasource + "\' and a.engine=\'" + engine + "\' and a.user=\'" + userName + "\' LIMIT " + limit);
+                retVal.put("hit", queryList.size());
             }
 
             List<List<Object>> queryHistoryList = new ArrayList<List<Object>>();
             for (Query query : queryList) {
-                if(query.getStatus().equals(Status.FAILED.name())) {
-                    continue;
-                }
                 List<Object> row = new ArrayList<>();
                 row.add(query.getQueryId());
                 row.add(query.getQueryString());
@@ -100,29 +100,12 @@ public class QueryHistoryUserServlet extends HttpServlet {
                 row.add(query.getExtraColumn("label_name"));
                 queryHistoryList.add(row);
             }
-            retVal.put("hit", queryHistoryList.size());
 
-            String offsetStr = request.getParameter("offset");
-            int offset = 0;
-            if(offsetStr != null && offsetStr.length() > 0) {
-                offset = Integer.parseInt(offsetStr);
-            }
-
-            String limitStr = request.getParameter("limit");
-            int limit = 100;
-            if(limitStr != null && limitStr.length() > 0) {
-                limit = Integer.parseInt(limitStr);
-            }
-
-            List<List<Object>> subQueryHistoryList = null;
             if(queryHistoryList.isEmpty()) {
-                subQueryHistoryList = Collections.emptyList();
-            } else if(offset + limit <= queryHistoryList.size()) {
-                subQueryHistoryList = queryHistoryList.subList(offset, offset + limit);
+                retVal.put("results", Collections.emptyList());
             } else {
-                subQueryHistoryList = queryHistoryList.subList(offset, queryHistoryList.size());
+                retVal.put("results", queryHistoryList);
             }
-            retVal.put("results", subQueryHistoryList);
 
             long totalCount = db.count(Query.class)
                     .where("datasource=?", datasource)
