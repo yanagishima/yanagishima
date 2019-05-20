e1febe80cad78fd520c0abc03bfce552f91889dc
wyukawa <wyukawa@users.noreply.github.com>
2019-05-20 09:35:34 +0900

    Update YanagishimaConfig.java

--- src/main/java/yanagishima/config/YanagishimaConfig.java
+++ src/main/java/yanagishima/config/YanagishimaConfig.java
@@ -403,7 +403,7 @@ public class YanagishimaConfig {
 		}
 	}
 
-  public boolean isHiveImpersonation(String datasource) {
+	public boolean isHiveImpersonation(String datasource) {
 		return Boolean.parseBoolean(Optional.ofNullable(properties.getProperty("hive.jdbc.impersonation." + datasource)).orElse("false"));
 	}
 
