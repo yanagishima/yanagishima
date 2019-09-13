f2bc229497c38b13b37b44117a8d5b4a1036b058
Yuya Ebihara <ebyhry@gmail.com>
2019-09-13 19:46:46 +0900

    Minor refactoring about convert-hive and presto servlet

--- src/main/java/yanagishima/servlet/ConvertHiveServlet.java
+++ src/main/java/yanagishima/servlet/ConvertHiveServlet.java
@@ -1,44 +1,26 @@
 package yanagishima.servlet;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import yanagishima.util.JsonUtil;
+import static yanagishima.util.JsonUtil.writeJSON;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Optional;
 
 import javax.inject.Singleton;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Optional;
 
 @Singleton
 public class ConvertHiveServlet extends HttpServlet {
-
-	private static Logger LOGGER = LoggerFactory.getLogger(ConvertHiveServlet.class);
-
 	private static final long serialVersionUID = 1L;
 
 	@Override
-	protected void doPost(HttpServletRequest request,
-						  HttpServletResponse response) throws ServletException, IOException {
-
-		HashMap<String, Object> retVal = new HashMap<String, Object>();
-
-		try {
-			Optional<String> queryOptional = Optional.ofNullable(request.getParameter("query"));
-			queryOptional.ifPresent(query -> {
-				String hiveQuery = query.replace("json_extract_scalar", "get_json_object").replace("cross join unnest", "lateral view explode");
-				retVal.put("hiveQuery", hiveQuery);
-			});
-		} catch (Throwable e) {
-			LOGGER.error(e.getMessage(), e);
-			retVal.put("error", e.getMessage());
-		}
-
-		JsonUtil.writeJSON(response, retVal);
-
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Optional.ofNullable(request.getParameter("query")).ifPresent(query -> {
+			String replacedHiveQuery = query.replace("json_extract_scalar", "get_json_object").replace("cross join unnest", "lateral view explode");
+			writeJSON(response, Map.of("hiveQuery", replacedHiveQuery));
+		});
 	}
-
 }
--- src/main/java/yanagishima/servlet/ConvertPrestoServlet.java
+++ src/main/java/yanagishima/servlet/ConvertPrestoServlet.java
@@ -1,44 +1,26 @@
 package yanagishima.servlet;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import yanagishima.util.JsonUtil;
+import static yanagishima.util.JsonUtil.writeJSON;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Optional;
 
 import javax.inject.Singleton;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Optional;
 
 @Singleton
 public class ConvertPrestoServlet extends HttpServlet {
-
-	private static Logger LOGGER = LoggerFactory.getLogger(ConvertPrestoServlet.class);
-
 	private static final long serialVersionUID = 1L;
 
 	@Override
-	protected void doPost(HttpServletRequest request,
-						  HttpServletResponse response) throws ServletException, IOException {
-
-		HashMap<String, Object> retVal = new HashMap<String, Object>();
-
-		try {
-			Optional<String> queryOptional = Optional.ofNullable(request.getParameter("query"));
-			queryOptional.ifPresent(query -> {
-				String prestoQuery = query.replace("get_json_object", "json_extract_scalar").replace("lateral view explode", "cross join unnest");
-				retVal.put("prestoQuery", prestoQuery);
-			});
-		} catch (Throwable e) {
-			LOGGER.error(e.getMessage(), e);
-			retVal.put("error", e.getMessage());
-		}
-
-		JsonUtil.writeJSON(response, retVal);
-
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Optional.ofNullable(request.getParameter("query")).ifPresent(query -> {
+			String replacedPrestoQuery = query.replace("get_json_object", "json_extract_scalar").replace("lateral view explode", "cross join unnest");
+			writeJSON(response, Map.of("prestoQuery", replacedPrestoQuery));
+		});
 	}
-
 }
