2c42de2cd0f3b0b4ae39f4176a525dad254f47d9
Yuya Ebihara <ebyhry@gmail.com>
2019-11-04 19:02:06 +0900

    Add Travis notification test

--- src/test/java/yanagishima/util/PathUtilTest.java
+++ src/test/java/yanagishima/util/PathUtilTest.java
@@ -22,7 +22,7 @@ public class PathUtilTest {
 
     @Test
     public void testGetResultFilePath() throws IOException {
-        Path expected = Paths.get(CURRENT_PATH, "result/test/20190102/20190102_abcdef.tsv");
+        Path expected = Paths.get(CURRENT_PATH, "result/test/20190102/20190102_abcdef2.tsv");
         Path actual = PathUtil.getResultFilePath("test", "20190102_abcdef", false);
         assertEquals(expected, actual);
         assertTrue(Files.deleteIfExists(actual.getParent()));
