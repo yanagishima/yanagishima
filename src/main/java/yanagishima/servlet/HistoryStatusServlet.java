31e04492eae66faba5d9dcc094c3579aca506f74
Yuya Ebihara <ebyhry@gmail.com>
2019-12-14 19:30:21 +0900

    Cleanup HistoryStatusServlet class

--- src/main/java/yanagishima/servlet/HistoryStatusServlet.java
+++ src/main/java/yanagishima/servlet/HistoryStatusServlet.java
@@ -1,79 +1,70 @@
 package yanagishima.servlet;
 
-import me.geso.tinyorm.TinyORM;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import yanagishima.config.YanagishimaConfig;
-import yanagishima.row.Query;
-import yanagishima.util.JsonUtil;
-
-import javax.inject.Inject;
-import javax.inject.Singleton;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Optional;
-
 import static yanagishima.util.AccessControlUtil.sendForbiddenError;
 import static yanagishima.util.AccessControlUtil.validateDatasource;
 import static yanagishima.util.HttpRequestUtil.getRequiredParameter;
+import static yanagishima.util.JsonUtil.writeJSON;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import me.geso.tinyorm.TinyORM;
+import yanagishima.config.YanagishimaConfig;
+import yanagishima.row.Query;
 
 @Singleton
 public class HistoryStatusServlet extends HttpServlet {
-
-    private static Logger LOGGER = LoggerFactory
-            .getLogger(HistoryStatusServlet.class);
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(HistoryStatusServlet.class);
     private static final long serialVersionUID = 1L;
 
-    @Inject
+    private YanagishimaConfig config;
     private TinyORM db;
 
-    private YanagishimaConfig yanagishimaConfig;
-
     @Inject
-    public HistoryStatusServlet(YanagishimaConfig yanagishimaConfig) {
-        this.yanagishimaConfig = yanagishimaConfig;
+    public HistoryStatusServlet(YanagishimaConfig config, TinyORM db) {
+        this.config = config;
+        this.db = db;
     }
 
     @Override
-    protected void doGet(HttpServletRequest request,
-                         HttpServletResponse response) throws ServletException, IOException {
-
-        HashMap<String, Object> retVal = new HashMap<String, Object>();
-        retVal.put("status", "ng");
-
-        try {
-            Optional<String> queryidOptional = Optional.ofNullable(request.getParameter("queryid"));
-            if(queryidOptional.isPresent()) {
-                String datasource = getRequiredParameter(request, "datasource");
-                if (yanagishimaConfig.isCheckDatasource() && !validateDatasource(request, datasource)) {
-                    sendForbiddenError(response);
-                    return;
-                }
-                String engine = request.getParameter("engine");
-                Optional<Query> queryOptional;
-                if(engine == null) {
-                    queryOptional = db.single(Query.class).where("query_id=? and datasource=?", queryidOptional.get(), datasource).execute();
-                } else {
-                    queryOptional = db.single(Query.class).where("query_id=? and datasource=? and engine=?", queryidOptional.get(), datasource, engine).execute();
-                }
-                queryOptional.ifPresent(query -> {
-                    retVal.put("status", "ok");
-                });
-            }
-        } catch (Throwable e) {
-            LOGGER.error(e.getMessage(), e);
-            retVal.put("error", e.getMessage());
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+        Map<String, Object> responseBody = new HashMap<>();
+        responseBody.put("status", "ng");
+        String queryId = request.getParameter("queryid");
+        if (queryId == null) {
+            writeJSON(response, responseBody);
+            return;
         }
 
-        JsonUtil.writeJSON(response, retVal);
-
+        try {
+            String datasource = getRequiredParameter(request, "datasource");
+            String engine = request.getParameter("engine");
+            if (config.isCheckDatasource() && !validateDatasource(request, datasource)) {
+                sendForbiddenError(response);
+                return;
+            }
+            findQuery(datasource, engine, queryId).ifPresent(query -> responseBody.put("status", "ok"));
+        } catch (Throwable e) {
+            LOGGER.error(e.getMessage(), e);
+            responseBody.put("error", e.getMessage());
+        }
+        writeJSON(response, responseBody);
     }
 
-
-
+    private Optional<Query> findQuery(String datasource, String engine, String queryId) {
+        if (engine == null) {
+            return db.single(Query.class).where("query_id=? and datasource=?", queryId, datasource).execute();
+        }
+        return db.single(Query.class).where("query_id=? and datasource=? and engine=?", queryId, datasource, engine).execute();
+    }
 }
