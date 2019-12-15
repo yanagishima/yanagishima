4e4e5137574c507b29daf8b37e96a7d988116d0a
Yuya Ebihara <ebyhry@gmail.com>
2019-12-16 00:30:10 +0900

    Remove redundant condition to check directory existence

--- src/main/java/yanagishima/util/PathUtil.java
+++ src/main/java/yanagishima/util/PathUtil.java
@@ -14,10 +14,7 @@ public final class PathUtil {
 	public static Path getResultFilePath(String datasource, String queryId, boolean error) {
 		String date = queryId.substring(0, 8);
 		File directory = new File(format("%s/result/%s/%s", CURRENT_PATH, datasource, date));
-		if (!directory.exists()) {
-			directory.mkdirs();
-		}
-
+		directory.mkdirs();
 		String extension = error ? "err" : "tsv";
 		return Paths.get(format("%s/%s.%s", directory, queryId, extension));
 	}
