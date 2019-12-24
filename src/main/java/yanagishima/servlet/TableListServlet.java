3e0cdc93ab12366c4b4b7e1a68db16890ae0640e
Yuya Ebihara <ebyhry@gmail.com>
2019-12-24 22:32:30 +0900

    Format TableListServlet class

--- src/main/java/yanagishima/servlet/TableListServlet.java
+++ src/main/java/yanagishima/servlet/TableListServlet.java
@@ -1,11 +1,19 @@
 package yanagishima.servlet;
 
-import io.prestosql.client.ClientException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import yanagishima.config.YanagishimaConfig;
-import yanagishima.service.PrestoService;
-import yanagishima.util.JsonUtil;
+import static java.lang.String.format;
+import static java.lang.String.join;
+import static yanagishima.util.AccessControlUtil.sendForbiddenError;
+import static yanagishima.util.AccessControlUtil.validateDatasource;
+import static yanagishima.util.Constants.YANAGISHIMA_COMMENT;
+import static yanagishima.util.HttpRequestUtil.getRequiredParameter;
+import static yanagishima.util.JsonUtil.writeJSON;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -13,100 +21,90 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
-import static yanagishima.util.AccessControlUtil.sendForbiddenError;
-import static yanagishima.util.AccessControlUtil.validateDatasource;
-import static yanagishima.util.Constants.YANAGISHIMA_COMMENT;
-import static yanagishima.util.HttpRequestUtil.getRequiredParameter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.prestosql.client.ClientException;
+import yanagishima.config.YanagishimaConfig;
+import yanagishima.service.PrestoService;
 
 @Singleton
 public class TableListServlet extends HttpServlet {
-
-    private static final Logger LOGGER = LoggerFactory
-            .getLogger(TableListServlet.class);
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(TableListServlet.class);
     private static final long serialVersionUID = 1L;
 
-    private PrestoService prestoService;
-
-    private YanagishimaConfig yanagishimaConfig;
+    private final PrestoService prestoService;
+    private final YanagishimaConfig config;
 
     @Inject
-    public TableListServlet(PrestoService prestoService, YanagishimaConfig yanagishimaConfig) {
+    public TableListServlet(PrestoService prestoService, YanagishimaConfig config) {
         this.prestoService = prestoService;
-        this.yanagishimaConfig = yanagishimaConfig;
+        this.config = config;
     }
 
     @Override
-    protected void doPost(HttpServletRequest request,
-                         HttpServletResponse response) throws ServletException, IOException {
-
-        HashMap<String, Object> retVal = new HashMap<String, Object>();
-
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Map<String, Object> responseBody = new HashMap<>();
         try {
-
             String datasource = getRequiredParameter(request, "datasource");
-            if (yanagishimaConfig.isCheckDatasource() && !validateDatasource(request, datasource)) {
+            if (config.isCheckDatasource() && !validateDatasource(request, datasource)) {
                 sendForbiddenError(response);
                 return;
             }
 
-            String prestoCoordinatorServer = yanagishimaConfig.getPrestoCoordinatorServerOrNull(datasource);
-            if (prestoCoordinatorServer == null) {
-                JsonUtil.writeJSON(response, retVal);
+            String coordinatorServer = config.getPrestoCoordinatorServerOrNull(datasource);
+            if (coordinatorServer == null) {
+                writeJSON(response, responseBody);
                 return;
             }
 
             String catalog = getRequiredParameter(request, "catalog");
-            String userName = null;
+            String userName = getUsername(request);
             Optional<String> prestoUser = Optional.ofNullable(request.getParameter("user"));
             Optional<String> prestoPassword = Optional.ofNullable(request.getParameter("password"));
-            if (yanagishimaConfig.isUseAuditHttpHeaderName()) {
-                userName = request.getHeader(yanagishimaConfig.getAuditHttpHeaderName());
-            } else {
-                if (prestoUser.isPresent() && prestoPassword.isPresent()) {
-                    userName = prestoUser.get();
-                }
+            if (prestoUser.isPresent() && prestoPassword.isPresent() && prestoUser.get().isEmpty()) {
+                responseBody.put("error", "user is empty");
+                writeJSON(response, responseBody);
+                return;
             }
-            if (prestoUser.isPresent() && prestoPassword.isPresent()) {
-                if (prestoUser.get().length() == 0) {
-                    retVal.put("error", "user is empty");
-                    JsonUtil.writeJSON(response, retVal);
-                    return;
-                }
-            }
-            List<String> invisibleSchemas = yanagishimaConfig.getInvisibleSchemas(datasource, catalog);
-            String notin = "('" + String.join("','", invisibleSchemas) + "')";
-            String query = String.format("%sSELECT table_catalog || '.' || table_schema || '.' || table_name FROM %s.information_schema.tables WHERE table_schema NOT IN %s",
-                                         YANAGISHIMA_COMMENT, catalog, notin);
+            List<String> invisibleSchemas = config.getInvisibleSchemas(datasource, catalog);
+            String query = format("%sSELECT table_catalog || '.' || table_schema || '.' || table_name "
+                                  + "FROM %s.information_schema.tables "
+                                  + "WHERE table_schema NOT IN ('%s')",
+                                  YANAGISHIMA_COMMENT, catalog, join("','", invisibleSchemas));
             try {
                 List<String> tables = prestoService.doQuery(datasource, query, userName, prestoUser, prestoPassword, false, Integer.MAX_VALUE).getRecords().stream()
                                                    .map(list -> list.get(0))
                                                    .collect(Collectors.toList());
-                retVal.put("tableList", tables);
+                responseBody.put("tableList", tables);
             } catch (ClientException e) {
                 if (prestoUser.isPresent()) {
-                    LOGGER.error(String.format("%s failed to be authenticated", prestoUser.get()));
+                    LOGGER.error(format("%s failed to be authenticated", prestoUser.get()));
                 }
                 LOGGER.error(e.getMessage(), e);
-                retVal.put("error", e.getMessage());
+                responseBody.put("error", e.getMessage());
             } catch (Throwable e) {
                 LOGGER.error(e.getMessage(), e);
-                retVal.put("error", e.getMessage());
+                responseBody.put("error", e.getMessage());
             }
 
         } catch (Throwable e) {
             LOGGER.error(e.getMessage(), e);
-            retVal.put("error", e.getMessage());
+            responseBody.put("error", e.getMessage());
         }
-
-        JsonUtil.writeJSON(response, retVal);
-
+        writeJSON(response, responseBody);
     }
 
+    private String getUsername(HttpServletRequest request) {
+        if (config.isUseAuditHttpHeaderName()) {
+            return request.getHeader(config.getAuditHttpHeaderName());
+        }
+        String user = request.getParameter("user");
+        String password = request.getParameter("password");
+        if (user != null && password != null) {
+            return user;
+        }
+        return null;
+    }
 }
