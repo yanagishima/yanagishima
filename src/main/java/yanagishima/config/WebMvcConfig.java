fcb295b89b285b2d059b8e7c528918a5a632b7f4
Yuya Ebihara <ebyhry@gmail.com>
2020-12-14 20:11:22 +0900

    Use WebMvcConfigurer and add .gitkeep

--- src/main/java/yanagishima/config/WebMvcConfig.java
+++ src/main/java/yanagishima/config/WebMvcConfig.java
@@ -4,12 +4,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = { "yanagishima.controller" })
-public class WebMvcConfig extends WebMvcConfigurerAdapter {
+public class WebMvcConfig implements WebMvcConfigurer {
   @Override
   public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
     configurer.enable("default");
