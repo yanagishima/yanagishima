b1540ba7da1df827e00864f90f9668b877c18ac2
Yuya Ebihara <ebyhry@gmail.com>
2020-12-15 16:32:00 +0900

    Introduce Spring and apply to health check (#185)
    * Introduce Spring and apply to health check
    
    * Use WebMvcConfigurer and add .gitkeep

--- src/main/java/yanagishima/module/PrestoServletModule.java
+++ src/main/java/yanagishima/module/PrestoServletModule.java
@@ -32,7 +32,6 @@ public class PrestoServletModule extends ServletModule {
 		bind(LabelServlet.class);
 		bind(StarredSchemaServlet.class);
 		bind(CheckPrestoQueryServlet.class);
-		bind(HealthCheckServlet.class);
 
 		serve("/presto").with(PrestoServlet.class);
 		serve("/prestoAsync").with(PrestoAsyncServlet.class);
@@ -60,6 +59,5 @@ public class PrestoServletModule extends ServletModule {
 		serve("/label").with(LabelServlet.class);
 		serve("/starredSchema").with(StarredSchemaServlet.class);
 		serve("/checkPrestoQuery").with(CheckPrestoQueryServlet.class);
-		serve("/healthCheck").with(HealthCheckServlet.class);
 	}
 }
