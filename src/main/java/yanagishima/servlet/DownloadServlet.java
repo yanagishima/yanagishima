01590e800b51085aed7f8f97aca3c4c5710f9560
Yuya Ebihara <ebyhry@gmail.com>
2020-12-16 18:52:58 +0900

    Migrate download servlet to spring (#210)

--- src/main/java/yanagishima/servlet/DownloadServlet.java
+++ src/main/java/yanagishima/servlet/DownloadServlet.java
@@ -3,13 +3,11 @@ package yanagishima.servlet;
 import yanagishima.config.YanagishimaConfig;
 import yanagishima.repository.TinyOrm;
 import yanagishima.model.db.Query;
-import yanagishima.model.HttpRequestContext;
 
 import javax.inject.Inject;
-import javax.inject.Singleton;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import java.util.Optional;
 
 import static java.util.Objects.requireNonNull;
@@ -17,43 +15,50 @@ import static yanagishima.util.AccessControlUtil.sendForbiddenError;
 import static yanagishima.util.AccessControlUtil.validateDatasource;
 import static yanagishima.util.DownloadUtil.downloadTsv;
 
-@Singleton
-public class DownloadServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-    private final YanagishimaConfig config;
-    private final TinyOrm db;
+import com.google.inject.Injector;
 
-    @Inject
-    public DownloadServlet(YanagishimaConfig config, TinyOrm db) {
-        this.config = config;
-        this.db = db;
+@RestController
+public class DownloadServlet {
+  private final YanagishimaConfig config;
+  private final TinyOrm db;
+
+  @Inject
+  public DownloadServlet(Injector injector) {
+    this.config = injector.getInstance(YanagishimaConfig.class);
+    this.db = injector.getInstance(TinyOrm.class);
+  }
+
+  @GetMapping("download")
+  public void get(@RequestParam String datasource,
+                  @RequestParam(required = false) String queryid,
+                  @RequestParam(defaultValue = "UTF-8") String encode,
+                  @RequestParam(defaultValue = "true") boolean header,
+                  @RequestParam(defaultValue = "true") boolean bom,
+                  HttpServletRequest request, HttpServletResponse response) {
+    if (queryid == null) {
+      return;
     }
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-        HttpRequestContext context = new HttpRequestContext(request);
-        if (context.getQueryId() == null) {
-            return;
-        }
-
-        requireNonNull(context.getDatasource(), "datasource is null");
-        if (config.isCheckDatasource() && !validateDatasource(request, context.getDatasource())) {
-            sendForbiddenError(response);
-            return;
-        }
-
-        String fileName = context.getQueryId() + ".tsv";
-        if (config.isAllowOtherReadResult(context.getDatasource())) {
-            downloadTsv(response, fileName, context.getDatasource(), context.getQueryId(), context.getEncode(), context.isShowHeader(), context.isShowBom());
-            return;
-        }
-        String user = request.getHeader(config.getAuditHttpHeaderName());
-        requireNonNull(user, "Username must exist when auditing header name is enabled");
-        Optional<Query> query = db.singleQuery("query_id = ? AND datasource = ? AND user = ?", context.getQueryId(), context.getDatasource(), user);
-        if (query.isPresent()) {
-            downloadTsv(response, fileName, context.getDatasource(), context.getQueryId(), context.getEncode(), context.isShowHeader(), context.isShowBom());
-        }
+    if (config.isCheckDatasource() && !validateDatasource(request, datasource)) {
+      sendForbiddenError(response);
+      return;
     }
+
+    String fileName = queryid + ".tsv";
+    if (config.isAllowOtherReadResult(datasource)) {
+      downloadTsv(response, fileName, datasource, queryid, encode, header, bom);
+      return;
+    }
+    String user = request.getHeader(config.getAuditHttpHeaderName());
+    requireNonNull(user, "Username must exist when auditing header name is enabled");
+    Optional<Query> query = db.singleQuery("query_id = ? AND datasource = ? AND user = ?",
+                                           queryid, datasource, user);
+    if (query.isPresent()) {
+      downloadTsv(response, fileName, datasource, queryid, encode, header, bom);
+    }
+  }
 }
-
