a96e5082b5465f4793b54200bc80c82f32601eb6
Yuya Ebihara <ebyhry@gmail.com>
2020-12-16 11:25:20 +0900

    Migrate convert Presto servlet to spring (#207)

---
+++ src/main/java/yanagishima/model/dto/PrestoQueryDto.java
@@ -0,0 +1,10 @@
+package yanagishima.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PrestoQueryDto {
+  private final String prestoQuery;
+}
