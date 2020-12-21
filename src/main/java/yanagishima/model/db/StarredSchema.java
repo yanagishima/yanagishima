59f684923b62e297ca59a2a81290899bf03ae6bf
Yuya Ebihara <ebyhry@gmail.com>
2020-12-21 20:18:35 +0900

    Apply Spring Data JPA to starred schema

--- src/main/java/yanagishima/model/db/StarredSchema.java
+++ src/main/java/yanagishima/model/db/StarredSchema.java
@@ -1,36 +1,38 @@
 package yanagishima.model.db;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import me.geso.tinyorm.Row;
-import me.geso.tinyorm.annotations.Column;
-import me.geso.tinyorm.annotations.PrimaryKey;
-import me.geso.tinyorm.annotations.Table;
 
-@Table("starred_schema")
 @Data
-@EqualsAndHashCode(callSuper = false)
-public class StarredSchema extends Row<StarredSchema> {
-
-    @PrimaryKey
-    @Column("starred_schema_id")
+@Entity
+@Table(name = "starred_schema")
+public class StarredSchema {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "starred_schema_id")
     @JsonProperty("starred_schema_id")
     private int starredSchemaId;
 
-    @Column("datasource")
+    @Column(name = "datasource")
     private String datasource;
 
-    @Column("engine")
+    @Column(name = "engine")
     private String engine;
 
-    @Column("catalog")
+    @Column(name = "catalog")
     private String catalog;
 
-    @Column("schema")
+    @Column(name = "schema")
     private String schema;
 
-    @Column("user")
+    @Column(name = "user")
     private String user;
 }
