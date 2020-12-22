ad19272128990c05796ac0dcf038133ecc89d34f
Yuya Ebihara <ebyhry@gmail.com>
2020-12-22 09:23:10 +0900

    Migrate Presto async servlet to spring

--- src/main/java/yanagishima/module/PrestoServletModule.java
+++ src/main/java/yanagishima/module/PrestoServletModule.java
@@ -7,7 +7,6 @@ public class PrestoServletModule extends ServletModule {
 	@Override
 	protected void configureServlets() {
 		bind(PrestoServlet.class);
-		bind(PrestoAsyncServlet.class);
 		bind(QueryServlet.class);
 		bind(PrestoKillServlet.class);
 		bind(FormatSqlServlet.class);
@@ -24,7 +23,6 @@ public class PrestoServletModule extends ServletModule {
 		bind(CheckPrestoQueryServlet.class);
 
 		serve("/presto").with(PrestoServlet.class);
-		serve("/prestoAsync").with(PrestoAsyncServlet.class);
 		serve("/query").with(QueryServlet.class);
 		serve("/kill").with(PrestoKillServlet.class);
 		serve("/format").with(FormatSqlServlet.class);
