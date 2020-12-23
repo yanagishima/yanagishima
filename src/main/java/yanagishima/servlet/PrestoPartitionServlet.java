94a1c7c69c2b7be4bbdd0453fa090b18391fa4a4
Yuya Ebihara <ebyhry@gmail.com>
2020-12-23 10:09:20 +0900

    Migrate Presto partition servlet to spring

--- src/main/java/yanagishima/servlet/PrestoPartitionServlet.java
+++ src/main/java/yanagishima/servlet/PrestoPartitionServlet.java
@@ -8,8 +8,6 @@ import static java.util.Objects.requireNonNull;
 import static yanagishima.util.AccessControlUtil.sendForbiddenError;
 import static yanagishima.util.AccessControlUtil.validateDatasource;
 import static yanagishima.util.Constants.YANAGISHIMA_COMMENT;
-import static yanagishima.util.HttpRequestUtil.getRequiredParameter;
-import static yanagishima.util.JsonUtil.writeJSON;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -21,15 +19,16 @@ import java.util.Set;
 import java.util.TreeSet;
 
 import javax.annotation.Nullable;
-import javax.inject.Inject;
-import javax.inject.Singleton;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -39,42 +38,35 @@ import yanagishima.model.presto.PrestoQueryResult;
 import yanagishima.service.PrestoService;
 
 @Slf4j
-@Singleton
-public class PrestoPartitionServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+@RestController
+@RequiredArgsConstructor
+public class PrestoPartitionServlet {
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     private final PrestoService prestoService;
     private final YanagishimaConfig config;
 
-    @Inject
-    public PrestoPartitionServlet(PrestoService prestoService, YanagishimaConfig config) {
-        this.prestoService = prestoService;
-        this.config = config;
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    @PostMapping("prestoPartition")
+    public Map<String, Object> post(@RequestParam String datasource,
+                                    @RequestParam String catalog,
+                                    @RequestParam String schema,
+                                    @RequestParam String table,
+                                    @RequestParam(required = false) String partitionColumn,
+                                    @RequestParam(required = false) String partitionColumnType,
+                                    @RequestParam(required = false) String partitionValue,
+                                    @RequestParam(name = "user") Optional<String> prestoUser,
+                                    @RequestParam(name = "password") Optional<String> prestoPassword,
+                                    HttpServletRequest request, HttpServletResponse response) {
         Map<String, Object> responseBody = new HashMap<>();
         try {
-
-            String datasource = getRequiredParameter(request, "datasource");
             if (config.isCheckDatasource() && !validateDatasource(request, datasource)) {
                 sendForbiddenError(response);
-                return;
+                return responseBody;
             }
 
             String user = getUsername(request);
-            Optional<String> prestoUser = Optional.ofNullable(request.getParameter("user"));
-            Optional<String> prestoPassword = Optional.ofNullable(request.getParameter("password"));
             Optional<String> webHdfsProxyUser = config.getWebhdfsProxyUser(datasource);
             Optional<String> webHdfsProxyPassword = config.getWebhdfsProxyPassword(datasource);
-            String catalog = getRequiredParameter(request, "catalog");
-            String schema = getRequiredParameter(request, "schema");
-            String table = getRequiredParameter(request, "table");
-            String partitionColumn = request.getParameter("partitionColumn");
-            String partitionColumnType = request.getParameter("partitionColumnType");
-            String partitionValue = request.getParameter("partitionValue");
             if (partitionColumn == null || partitionValue == null) {
                 Optional<String> webhdfsUrl = config.getWebhdfsUrl(datasource, catalog, schema, table);
                 if (webhdfsUrl.isPresent()) {
@@ -159,7 +151,7 @@ public class PrestoPartitionServlet extends HttpServlet {
             log.error(e.getMessage(), e);
             responseBody.put("error", e.getMessage());
         }
-        writeJSON(response, responseBody);
+        return responseBody;
     }
 
     private void setFirstPartitionWithWebHdfs(Map<String, Object> responseBody, Optional<String> prestoUser, Optional<String> prestoPassword, String webHdfsUrl) throws IOException {
