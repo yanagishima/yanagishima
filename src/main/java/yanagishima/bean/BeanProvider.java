e029eb7ca2bf65fadf7afd349ba021bb3108bbef
Yuya Ebihara <ebyhry@gmail.com>
2020-12-23 18:10:44 +0900

    Manage Elasticsearch service in Spring DI (#240)
    Co-authored-by: Wataru Yukawa <wyukawa@users.noreply.github.com>

--- src/main/java/yanagishima/bean/BeanProvider.java
+++ src/main/java/yanagishima/bean/BeanProvider.java
@@ -8,14 +8,12 @@ import com.google.inject.Injector;
 
 import yanagishima.config.YanagishimaConfig;
 import yanagishima.repository.TinyOrm;
-import yanagishima.service.ElasticsearchService;
 import yanagishima.service.HiveService;
 import yanagishima.service.OldPrestoService;
 import yanagishima.service.PrestoService;
 
 @Component
 public class BeanProvider {
-  private final ElasticsearchService elasticsearchService;
   private final PrestoService prestoService;
   private final OldPrestoService oldPrestoService;
   private final HiveService hiveService;
@@ -24,17 +22,11 @@ public class BeanProvider {
 
   @Autowired
   public BeanProvider(Injector injector) {
-    this.elasticsearchService = injector.getInstance(ElasticsearchService.class);
     this.prestoService = injector.getInstance(PrestoService.class);
     this.oldPrestoService = injector.getInstance(OldPrestoService.class);
     this.hiveService = injector.getInstance(HiveService.class);
     this.config = injector.getInstance(YanagishimaConfig.class);
     this.db = injector.getInstance(TinyOrm.class);
-  }
-
-  @Bean
-  public ElasticsearchService elasticsearchService() {
-    return elasticsearchService;
   }
 
   @Bean
