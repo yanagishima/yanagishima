6a036ff6464382fe9b57c6c69d530dde99c7e342
Yuya Ebihara <ebyhry@gmail.com>
2020-12-23 16:07:38 +0900

    Migrate Presto partition servlet to spring (#235)

--- src/main/java/yanagishima/module/PrestoServletModule.java
+++ src/main/java/yanagishima/module/PrestoServletModule.java
@@ -16,7 +16,6 @@ public class PrestoServletModule extends ServletModule {
 		bind(QueryHistoryUserServlet.class);
 		bind(DatasourceAuthServlet.class);
 		bind(QueryStatusServlet.class);
-		bind(PrestoPartitionServlet.class);
 		bind(CommentServlet.class);
 		bind(LabelServlet.class);
 		bind(CheckPrestoQueryServlet.class);
@@ -31,7 +30,6 @@ public class PrestoServletModule extends ServletModule {
 		serve("/queryHistoryUser").with(QueryHistoryUserServlet.class);
 		serve("/datasourceAuth").with(DatasourceAuthServlet.class);
 		serve("/queryStatus").with(QueryStatusServlet.class);
-		serve("/prestoPartition").with(PrestoPartitionServlet.class);
 		serve("/comment").with(CommentServlet.class);
 		serve("/label").with(LabelServlet.class);
 		serve("/checkPrestoQuery").with(CheckPrestoQueryServlet.class);
