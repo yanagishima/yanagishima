ca191fc913db789189e3e88fd145a5b76c3b0f67
Yuya Ebihara <ebyhry@gmail.com>
2020-12-24 16:07:09 +0900

    Migrate query history servlet to spring (#251)

--- src/main/java/yanagishima/module/PrestoServletModule.java
+++ src/main/java/yanagishima/module/PrestoServletModule.java
@@ -6,10 +6,8 @@ import yanagishima.servlet.*;
 public class PrestoServletModule extends ServletModule {
 	@Override
 	protected void configureServlets() {
-		bind(QueryHistoryServlet.class);
 		bind(LabelServlet.class);
 
-		serve("/queryHistory").with(QueryHistoryServlet.class);
 		serve("/label").with(LabelServlet.class);
 	}
 }
