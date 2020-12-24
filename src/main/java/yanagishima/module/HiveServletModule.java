af22f327361d095d5b51e2590943c02158a15836
Yuya Ebihara <ebyhry@gmail.com>
2020-12-24 14:32:03 +0900

    Migrate Spark job list servlet to spring

--- src/main/java/yanagishima/module/HiveServletModule.java
+++ src/main/java/yanagishima/module/HiveServletModule.java
@@ -7,12 +7,10 @@ public class HiveServletModule extends ServletModule {
 	@Override
 	protected void configureServlets() {
 		bind(YarnJobListServlet.class);
-		bind(SparkJobListServlet.class);
 		bind(HiveQueryStatusServlet.class);
 		bind(HiveQueryDetailServlet.class);
 
 		serve("/yarnJobList").with(YarnJobListServlet.class);
-		serve("/sparkJobList").with(SparkJobListServlet.class);
 		serve("/hiveQueryStatus").with(HiveQueryStatusServlet.class);
 		serve("/sparkQueryStatus").with(HiveQueryStatusServlet.class);
 		serve("/hiveQueryDetail").with(HiveQueryDetailServlet.class);
