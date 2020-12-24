93f2a0058c406b73c7c94ff50107ee27255ec5ce
Yuya Ebihara <ebyhry@gmail.com>
2020-12-24 14:22:43 +0900

    Migrate query status servlet to spring (#244)

--- src/main/java/yanagishima/servlet/QueryStatusServlet.java
+++ src/main/java/yanagishima/servlet/QueryStatusServlet.java
@@ -4,22 +4,21 @@ import static io.prestosql.client.OkHttpUtil.basicAuth;
 import static java.lang.String.format;
 import static yanagishima.util.AccessControlUtil.sendForbiddenError;
 import static yanagishima.util.AccessControlUtil.validateDatasource;
-import static yanagishima.util.HttpRequestUtil.getRequiredParameter;
-import static yanagishima.util.JsonUtil.writeJSON;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.inject.Inject;
-import javax.inject.Singleton;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -27,28 +26,23 @@ import okhttp3.Response;
 import yanagishima.config.YanagishimaConfig;
 
 @Slf4j
-@Singleton
-public class QueryStatusServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+@RestController
+@RequiredArgsConstructor
+public class QueryStatusServlet {
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
-	private final YanagishimaConfig config;
-	private final OkHttpClient httpClient = new OkHttpClient();
+    private final YanagishimaConfig config;
+    private final OkHttpClient httpClient = new OkHttpClient();
 
-	@Inject
-	public QueryStatusServlet(YanagishimaConfig config) {
-		this.config = config;
-	}
-
-	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String datasource = getRequiredParameter(request, "datasource");
+    @PostMapping("queryStatus")
+    public Map<?, ?> post(@RequestParam String datasource,
+                          @RequestParam(name = "queryid", required = false) String queryId,
+                          HttpServletRequest request, HttpServletResponse response) throws IOException {
 		if (config.isCheckDatasource() && !validateDatasource(request, datasource)) {
 			sendForbiddenError(response);
-			return;
+			return Map.of();
 		}
 
-		String queryId = request.getParameter("queryid");
 		String coordinatorServer = config.getPrestoCoordinatorServer(datasource);
 		String json;
 		String userName = request.getHeader(config.getAuditHttpHeaderName());
@@ -60,8 +54,7 @@ public class QueryStatusServlet extends HttpServlet {
 		}
 		try (Response prestoResponse = buildClient(request).newCall(prestoRequest).execute()) {
 			if (!prestoResponse.isSuccessful() || prestoResponse.body() == null) {
-				writeJSON(response, toMap(prestoResponse.code(), prestoResponse.message()));
-				return;
+				return toMap(prestoResponse.code(), prestoResponse.message());
 			}
 			json = prestoResponse.body().string();
 		}
@@ -76,7 +69,7 @@ public class QueryStatusServlet extends HttpServlet {
 		}
 		status.remove("outputStage");
 		status.remove("session");
-		writeJSON(response, status);
+		return status;
 	}
 
 	private OkHttpClient buildClient(HttpServletRequest request) {
