87eb0e638610248ad58c401937e816ea0a2832d1
Yuya Ebihara <ebyhry@gmail.com>
2020-12-24 21:30:37 +0900

    Remove unused class and method

--- src/main/java/yanagishima/util/HttpRequestUtil.java
+++ src/main/java/yanagishima/util/HttpRequestUtil.java
@@ -8,24 +8,6 @@ import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public final class HttpRequestUtil {
-    public static String getOrDefaultParameter(HttpServletRequest request, String key, String defaultValue) {
-        String value = request.getParameter(key);
-        return value == null ? defaultValue : value;
-    }
-
-    public static boolean getOrDefaultParameter(HttpServletRequest request, String key, boolean defaultValue) {
-        String value = request.getParameter(key);
-        return value == null ? defaultValue : Boolean.parseBoolean(value);
-    }
-
-    public static String getRequiredParameter(HttpServletRequest request, String key) {
-        String value = request.getParameter(key);
-        if (value == null) {
-            throw new IllegalArgumentException(format("Missing required parameter '%s'", key));
-        }
-        return value;
-    }
-
     public static String getRequiredHeader(HttpServletRequest request, String key) {
         String value = request.getHeader(key);
         if (value == null) {
