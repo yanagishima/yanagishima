c489c436a7866680c97fc2fe47ba7c4a53376392
Yuya Ebihara <ebyhry@gmail.com>
2020-12-24 11:33:21 +0900

    Migrate query status servlet to spring

--- src/main/java/yanagishima/module/PrestoServletModule.java
+++ src/main/java/yanagishima/module/PrestoServletModule.java
@@ -8,12 +8,10 @@ public class PrestoServletModule extends ServletModule {
 	protected void configureServlets() {
 		bind(QueryHistoryServlet.class);
 		bind(QueryHistoryUserServlet.class);
-		bind(QueryStatusServlet.class);
 		bind(LabelServlet.class);
 
 		serve("/queryHistory").with(QueryHistoryServlet.class);
 		serve("/queryHistoryUser").with(QueryHistoryUserServlet.class);
-		serve("/queryStatus").with(QueryStatusServlet.class);
 		serve("/label").with(LabelServlet.class);
 	}
 }
