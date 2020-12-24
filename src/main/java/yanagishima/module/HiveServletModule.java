a153fcd15caefec5bf0f80b06de07b7068d69b1a
Yuya Ebihara <ebyhry@gmail.com>
2020-12-24 15:09:33 +0900

    Migrate Hive query detail servlet to spring (#247)

--- src/main/java/yanagishima/module/HiveServletModule.java
+++ src/main/java/yanagishima/module/HiveServletModule.java
@@ -9,13 +9,10 @@ public class HiveServletModule extends ServletModule {
 		bind(YarnJobListServlet.class);
 		bind(SparkJobListServlet.class);
 		bind(HiveQueryStatusServlet.class);
-		bind(HiveQueryDetailServlet.class);
 
 		serve("/yarnJobList").with(YarnJobListServlet.class);
 		serve("/sparkJobList").with(SparkJobListServlet.class);
 		serve("/hiveQueryStatus").with(HiveQueryStatusServlet.class);
 		serve("/sparkQueryStatus").with(HiveQueryStatusServlet.class);
-		serve("/hiveQueryDetail").with(HiveQueryDetailServlet.class);
-		serve("/sparkQueryDetail").with(HiveQueryDetailServlet.class);
 	}
 }
