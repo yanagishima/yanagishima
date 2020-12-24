aacaa9b30e18f71c928ed78afde8e4c3ae446fd7
Yuya Ebihara <ebyhry@gmail.com>
2020-12-24 15:02:44 +0900

    Migrate query history servlet to spring

--- src/main/java/yanagishima/servlet/QueryHistoryServlet.java
+++ src/main/java/yanagishima/servlet/QueryHistoryServlet.java
@@ -1,19 +1,15 @@
 package yanagishima.servlet;
 
 import io.airlift.units.DataSize;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import yanagishima.config.YanagishimaConfig;
 import yanagishima.repository.TinyOrm;
 import yanagishima.model.db.Query;
 
 import javax.annotation.Nullable;
-import javax.inject.Inject;
-import javax.inject.Singleton;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -22,40 +18,31 @@ import static java.lang.String.join;
 import static java.util.Collections.nCopies;
 import static yanagishima.util.AccessControlUtil.sendForbiddenError;
 import static yanagishima.util.AccessControlUtil.validateDatasource;
-import static yanagishima.util.HttpRequestUtil.getRequiredParameter;
-import static yanagishima.util.JsonUtil.writeJSON;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@Singleton
-public class QueryHistoryServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-
+@RestController
+@RequiredArgsConstructor
+public class QueryHistoryServlet {
     private final YanagishimaConfig config;
     private final TinyOrm db;
 
-    @Inject
-    public QueryHistoryServlet(YanagishimaConfig config, TinyOrm db) {
-        this.config = config;
-        this.db = db;
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doGet(request, response);
-    }
-
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+    @RequestMapping(value = "/queryHistory", method = { RequestMethod.GET, RequestMethod.POST })
+    public Map<String, Object> get(@RequestParam String datasource,
+                                   @RequestParam(required = false) String queryids,
+                                   @RequestParam(required = false) String label,
+                                   HttpServletRequest request, HttpServletResponse response) {
         Map<String, Object> responseBody = new HashMap<>();
         try {
-            String datasource = getRequiredParameter(request, "datasource");
             if (config.isCheckDatasource() && !validateDatasource(request, datasource)) {
                 sendForbiddenError(response);
-                return;
+                return responseBody;
             }
-            String[] queryIds = request.getParameter("queryids").split(",");
-            String label = request.getParameter("label");
-
+            String[] queryIds = queryids.split(","); // TODO: Fix NPE
             responseBody.put("headers", Arrays.asList("Id", "Query", "Time", "rawDataSize", "engine", "finishedTime", "linenumber", "labelName", "status"));
             responseBody.put("results", getHistories(label, datasource, queryIds));
 
@@ -63,7 +50,7 @@ public class QueryHistoryServlet extends HttpServlet {
             log.error(e.getMessage(), e);
             responseBody.put("error", e.getMessage());
         }
-        writeJSON(response, responseBody);
+        return responseBody;
     }
 
     private List<List<Object>> getHistories(String label, String datasource, String[] queryIds) {
