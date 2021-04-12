e844063d45f8d61b312f3462a726b4464552d5c2
wyukawa <wyukawa@gmail.com>
2021-04-12 12:41:36 +0900

    change scope

--- src/test/java/yanagishima/util/PublishUtilTest.java
+++ src/test/java/yanagishima/util/PublishUtilTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
-public class PublishUtilTest {
+class PublishUtilTest {
 
     @Test
     void testCanAccessPublishedPage() {
