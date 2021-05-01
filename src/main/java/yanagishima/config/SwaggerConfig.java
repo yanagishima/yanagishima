021798aff6813bac8bf9fe3b719bec0bb198902f
Yuya Ebihara <ebyhry@gmail.com>
2021-05-01 17:37:44 +0900

    Upgrade Spring Fox to 3.0.0 (#320)
    Additonally, change to Open API from Swagger 2.

--- src/main/java/yanagishima/config/SwaggerConfig.java
+++ src/main/java/yanagishima/config/SwaggerConfig.java
@@ -8,14 +8,12 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
   @Bean
   public Docket swaggerSpringMvcPlugin() {
-    return new Docket(DocumentationType.SWAGGER_2)
+    return new Docket(DocumentationType.OAS_30)
         .useDefaultResponseMessages(false)
         .select()
         .apis(RequestHandlerSelectors.any())
