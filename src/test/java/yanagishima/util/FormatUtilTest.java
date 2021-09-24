9739c4bf7bce3d3a981ada1a65e0a98fd9d2e050
siu91 <gongshiwen@gmail.com>
2021-09-24 10:38:40 +0800

    Fix CI error and add test
    Signed-off-by: siu91 <gongshiwen@gmail.com>

--- src/test/java/yanagishima/util/FormatUtilTest.java
+++ src/test/java/yanagishima/util/FormatUtilTest.java
@@ -6,12 +6,14 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import org.junit.jupiter.api.Test;
 
 class FormatUtilTest {
-  @Test
-  void toSuccinctDataSize() {
-    assertNull(FormatUtil.toSuccinctDataSize(null));
-    assertEquals("1B", FormatUtil.toSuccinctDataSize(1));
-    assertEquals("1kB", FormatUtil.toSuccinctDataSize(1024));
-    assertEquals("1MB", FormatUtil.toSuccinctDataSize(1024 * 1024));
-    assertEquals("1GB", FormatUtil.toSuccinctDataSize(1024 * 1024 * 1024));
-  }
+    @Test
+    void toSuccinctDataSize() {
+        assertNull(FormatUtil.toSuccinctDataSize(null));
+        assertEquals("1B", FormatUtil.toSuccinctDataSize(1L));
+        assertEquals("1kB", FormatUtil.toSuccinctDataSize(1024L));
+        assertEquals("1MB", FormatUtil.toSuccinctDataSize((long) (1024 * 1024)));
+        assertEquals("1GB", FormatUtil.toSuccinctDataSize((long) (1024 * 1024 * 1024)));
+        assertEquals("2GB", FormatUtil.toSuccinctDataSize(2L * 1024 * 1024 * 1024));
+        assertEquals("4GB", FormatUtil.toSuccinctDataSize(4L * 1024 * 1024 * 1024));
+    }
 }
